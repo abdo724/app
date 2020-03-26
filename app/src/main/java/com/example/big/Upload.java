74de7b369466cb7dab6f0f806ce5219b2f5ccf1f
FORMAT <abdullrhmankhaled11@gmail.com>
2020-03-26 17:48:26 +0200

    app

---
+++ app/src/main/java/com/example/big/Upload.java
@@ -0,0 +1,30 @@
+package com.example.big;
+
+public class Upload {
+
+    String name , image_url;
+
+    public Upload() {
+    }
+
+    public Upload(String name, String image_url) {
+        this.name = name;
+        this.image_url = image_url;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getImage_url() {
+        return image_url;
+    }
+
+    public void setImage_url(String image_url) {
+        this.image_url = image_url;
+    }
+}
